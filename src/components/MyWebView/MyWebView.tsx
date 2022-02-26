438830d7d6c4bba5007bfecd78b3616f54a6058c
허재영 <jaeyoung.heo@qualson.com>
2022-02-26 17:40:02 +0900

    fix: MyWebView

--- src/components/MyWebView/MyWebView.tsx
+++ src/components/MyWebView/MyWebView.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import WebView from 'react-native-webview'
-import { useRecoilValue } from 'recoil'
-import { showSpaceAtom } from '@/modules/auth/atoms'
 
 function MyWebView() {
-  const showSpace = useRecoilValue(showSpaceAtom)
-
-  return showSpace ? <WebView source={{ uri: 'https://j-young-blog-frontend.vercel.app/play' }} /> : null
+  return <WebView source={{ uri: 'https://j-young-blog-frontend.vercel.app/play' }} />
 }
 
 export default MyWebView
