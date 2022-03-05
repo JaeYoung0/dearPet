c1b54b7315d3b37d21ca748f3da3f00a62be813a
허재영 <hjy8697@gmail.com>
2022-03-06 02:48:34 +0900

    fix: PostModel

--- src/server/posts/model.ts
+++ src/server/posts/model.ts
@@ -1,8 +1,12 @@
 import { UserModel } from '@/server/users/model'
 import { FirebaseFirestoreTypes } from '@react-native-firebase/firestore'
 export type PostModel = {
+  id: string
   user: UserModel
   title: string
   content: string
   createdAt: FirebaseFirestoreTypes.FieldValue
+
+  // firestore 복합색인도 타입에 추가
+  ['user.licenseId']?: string
 }
