e8b2c8eb951915f13e613ad18de9f035d8c9d14f
JaeYoung0 <hjy8697@gmail.com>
2022-03-05 23:21:53 +0900

    refactor: user service payload

--- src/server/users/service.ts
+++ src/server/users/service.ts
@@ -4,19 +4,19 @@ import { CreateUserDto, UpdateUserPhotoUrlDto } from './dto'
 
 const usersCollection = firestore().collection<UserModel>('Users')
 
-export function createUser(command: CreateUserDto) {
-  const { licenseId } = command
-  return usersCollection.doc(licenseId).set(command)
-}
-
 export async function getUser(id: string) {
   const doc = await usersCollection.doc(id).get()
 
   return doc.data()
 }
 
-export function updateUserPhotoUrl(command: UpdateUserPhotoUrlDto) {
-  const { licenseId, photoURL } = command
+export function createUser(payload: CreateUserDto) {
+  const { licenseId } = payload
+  return usersCollection.doc(licenseId).set(payload)
+}
+
+export function updateUserPhotoUrl(payload: UpdateUserPhotoUrlDto) {
+  const { licenseId, photoURL } = payload
 
   usersCollection.doc(licenseId).update({
     photoURL,
