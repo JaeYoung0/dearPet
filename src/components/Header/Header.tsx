dd9097adb607ac54f782e4dbc0ff612a85be9f2c
JaeYoung0 <hjy8697@gmail.com>
2022-03-05 23:17:21 +0900

    feat: Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import useCustomNavi from '@/hooks/useCustomNavi'
+import React from 'react'
+import { Pressable, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import MyIcons from '../MyIcons'
+
+type Props = {
+  title: string
+  onSave: () => void
+}
+function Header({ title, onSave }: Props) {
+  const navigation = useCustomNavi()
+  return (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Pressable
+          style={{ marginRight: 20 }}
+          onPress={() => {
+            navigation.goBack()
+          }}
+        >
+          <MyIcons name='Arrow' />
+        </Pressable>
+        <Text style={{ fontSize: 16 }}>{title}</Text>
+      </View>
+
+      <Pressable onPress={onSave}>
+        <MyIcons name='Check' color='#fff' />
+      </Pressable>
+    </View>
+  )
+}
+
+export default Header
