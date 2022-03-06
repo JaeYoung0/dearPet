b6b8063d8495c1c3778df5a02b6b8ba27c5103ad
JaeYoung0 <hjy8697@gmail.com>
2022-03-06 23:02:37 +0900

    chore: react-native-get-random-values

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native'
+import App from './App'
+import { name as appName } from './app.json'
+import 'react-native-get-random-values'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => App)
