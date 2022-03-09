fd26156179de999679818f19065a236d13db494e
JaeYoung0 <hjy8697@gmail.com>
2022-03-09 21:03:35 +0900

    chore: 임시이동버튼 삭제

--- src/screens/SpaceBox/SpaceBox.tsx
+++ src/screens/SpaceBox/SpaceBox.tsx
@@ -2,20 +2,10 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import WebView from 'react-native-webview'
 import BGMButton from '@/components/BGMButton'
-import { Alert, Button } from 'react-native'
-import useCustomNavi from '@/hooks/useCustomNavi'
 
 function SpaceBox() {
-  const navigation = useCustomNavi()
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#13141A' }}>
-      <Button
-        title='임시이동버튼'
-        onPress={() => {
-          navigation.navigate('MyPet', {})
-        }}
-      />
-
       <WebView source={{ uri: 'https://j-young-blog-frontend.vercel.app/play' }} />
       <BGMButton />
     </SafeAreaView>
