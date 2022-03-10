7d82dda62b57b98ddad76c3c2277050682c0ed2b
JaeYoung0 <hjy8697@gmail.com>
2022-03-10 21:34:44 +0900

    feat: getHashKey in MainActivity

--- android/app/src/main/java/com/dearpet2/MainActivity.java
+++ android/app/src/main/java/com/dearpet2/MainActivity.java
@@ -4,6 +4,17 @@ import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 
+// for getHashKey function
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -17,7 +28,31 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState){
-    SplashScreen.show(this);
     super.onCreate(savedInstanceState);
+    SplashScreen.show(this);
+
+    // 디버깅 해시키
+    getHashKey();
   }
+
+  private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
 }
