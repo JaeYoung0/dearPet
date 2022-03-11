dfabc0e5a8e5c05383f84f53e81a4dedbf74ac73
허재영 <hjy8697@gmail.com>
2022-03-12 01:40:24 +0900

    feat: Welcome screen에 SofiaText 적용

--- src/screens/Welcome/Welcome.style.ts
+++ src/screens/Welcome/Welcome.style.ts
@@ -1,65 +1,53 @@
 import { StyleSheet } from 'react-native'
+import styled from '@emotion/native'
+import SofiaText from '@/components/SofiaText'
+
+export const Title = styled(SofiaText)({
+  color: '#0B0543',
+  fontSize: 25,
+  lineHeight: 25,
+  position: 'absolute',
+  left: 30,
+  top: 30,
+  zIndex: 1,
+})
+
+export const Text = styled(SofiaText)({
+  color: '#3C3292',
+  fontSize: 16,
+  lineHeight: 16,
+  position: 'absolute',
+  left: 30,
+  top: 100,
+  zIndex: 1,
+})
+
+export const BackgroundImg = styled.Image({
+  width: '100%',
+  height: '100%',
+  resizeMode: 'cover',
+})
+
+export const ButtonText = styled(SofiaText)({
+  color: '#fff',
+  fontSize: 16,
+})
+
+export const Touchable = styled.TouchableOpacity({
+  backgroundColor: '#7E71F3',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: 262,
+  height: 40,
+  borderRadius: 60,
+  position: 'absolute',
+  bottom: 24,
+  shadowColor: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+})
 
 // FIXME: 세팅하자
-// import styled from '@emotion/native'
 
 export const styles = StyleSheet.create({
-  wrapper: {},
-  slide1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'transparent',
-  },
-  title: {
-    color: '#000000',
-    fontSize: 20,
-    fontWeight: 'bold',
-    position: 'absolute',
-    left: 30,
-    top: 30,
-    zIndex: 1,
-  },
-  text: {
-    color: '#30165B',
-    fontSize: 12,
-    fontWeight: 'bold',
-    position: 'absolute',
-    left: 30,
-    top: 100,
-    zIndex: 1,
-  },
-
-  title2: {
-    color: '#fff',
-    fontSize: 20,
-    fontWeight: 'bold',
-    position: 'absolute',
-    left: 30,
-    top: 30,
-    zIndex: 1,
-  },
-
-  text2: { color: '#D0CCFA', fontSize: 12, fontWeight: 'bold', position: 'absolute', left: 30, top: 100, zIndex: 1 },
-
-  img: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-  },
-
   dot: {
     backgroundColor: 'transparent',
     borderColor: '#fff',
@@ -68,24 +56,6 @@ export const styles = StyleSheet.create({
 
   activeDot: {
     backgroundColor: '#fff',
-  },
-
-  button: {
-    backgroundColor: '#7E71F3',
-    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 262,
-    height: 40,
-    borderRadius: 60,
-    position: 'absolute',
-    bottom: 24,
-  },
-
-  buttonText: {
-    fontWeight: 'bold',
-    color: '#fff',
-    fontSize: 13,
   },
 
   buttonWrapper: {
--- src/screens/Welcome/Welcome.tsx
+++ src/screens/Welcome/Welcome.tsx
@@ -7,6 +7,7 @@ import useCustomNavi from '@/hooks/useCustomNavi'
 import { useRecoilValue } from 'recoil'
 import { userStatus } from '@/modules/user/atoms'
 import { setStorage } from '@/modules/storage/helper'
+import * as S from './Welcome.style'
 
 const Welcome = () => {
   const navigation = useCustomNavi()
@@ -15,35 +16,31 @@ const Welcome = () => {
     <>
       <Swiper
         loop={false}
-        style={styles.wrapper}
         showsButtons={false}
         dotStyle={styles.dot}
         paginationStyle={{ bottom: 80 }}
         activeDotStyle={styles.activeDot}
       >
-        <View style={styles.slide1}>
-          <Text style={styles.title}>떠나간 반려동물에게{'\n'}못다한 말이 남았나요?</Text>
-          <Text style={styles.text}>디어펫을 통해{'\n'}별이된 아이에게 편지를 남겨보세요.</Text>
-          <Image style={styles.img} source={require('@assets/images/welcome-bg-1.png')} />
+        <View>
+          <S.Title weight={400}>떠나간 반려동물에게{'\n'}못다한 말이 남았나요?</S.Title>
+          <S.Text>디어펫을 통해{'\n'}별이된 아이에게 편지를 남겨보세요.</S.Text>
+          <S.BackgroundImg source={require('@assets/images/welcome-bg-1.png')} />
         </View>
-        <View style={styles.slide2}>
-          <Text style={styles.title}>너에게 배운 사랑만큼{'\n'}더 성숙한 내가 될게.</Text>
-          <Text style={styles.text}>영원히 너를 잊지 않겠다는 약속과{'\n'}더 행복해지겠다는 다짐</Text>
-          <Image style={styles.img} source={require('@assets/images/welcome-bg-2.png')} />
+        <View>
+          <S.Title>너에게 배운 사랑만큼{'\n'}더 성숙한 내가 될게.</S.Title>
+          <S.Text>영원히 너를 잊지 않겠다는 약속과{'\n'}더 행복해지겠다는 다짐</S.Text>
+          <S.BackgroundImg source={require('@assets/images/welcome-bg-2.png')} />
         </View>
-        <View style={styles.slide3}>
-          <Text style={styles.title2}>어두워야 보이는 별처럼{'\n'}늘 우린 함께인거지?</Text>
-          <Text style={styles.text2}>
-            행복했던 추억과 메세지로{'\n'}오래도록 반려동물을 기억할 공간을 만들어보세요.
-          </Text>
-          <Image style={styles.img} source={require('@assets/images/welcome-bg-3.png')} />
+        <View>
+          <S.Title style={{ color: '#EBEDF2' }}>어두워야 보이는 별처럼{'\n'}늘 우린 함께인거지?</S.Title>
+          <S.Text style={{ color: '#B8B0FF' }}>행복했던 추억으로{'\n'}오래도록 기억할 공간을 만들어보세요.</S.Text>
+          <S.BackgroundImg source={require('@assets/images/welcome-bg-3.png')} />
         </View>
       </Swiper>
 
       {/* Button대신 TouchableOpacity를 사용 - Button은 안드로이드와 ios에서 다르게 보이기 때문*/}
       <View style={styles.buttonWrapper}>
-        <TouchableOpacity
-          style={styles.button}
+        <S.Touchable
           activeOpacity={0.9}
           onPress={() => {
             if (!user) {
@@ -54,8 +51,8 @@ const Welcome = () => {
             }
           }}
         >
-          <Text style={styles.buttonText}>시작하기</Text>
-        </TouchableOpacity>
+          <S.ButtonText>시작하기</S.ButtonText>
+        </S.Touchable>
       </View>
     </>
   )
