6d8e7e3cacee94a41ae9a4f703dd2637cbfe217f
허재영 <hjy8697@gmail.com>
2022-03-12 01:41:23 +0900

    feat: useMeState

--- src/modules/user/atoms.ts
+++ src/modules/user/atoms.ts
@@ -1,7 +1,14 @@
 import { atom } from 'recoil'
 import { UserModel } from '@/server/users/model'
+import { useRecoilState } from 'recoil'
 
 export const userStatus = atom<UserModel | null>({
   key: 'user',
   default: null,
 })
+
+export function useMeState() {
+  const [me, setMe] = useRecoilState(userStatus)
+
+  return { me, setMe }
+}
