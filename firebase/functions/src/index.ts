e8d0d0d651e94cdcb46e376e622f40d480301f55
허재영 <hjy8697@gmail.com>
2022-03-13 03:33:57 +0900

    feat: firebase functions customToken

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,28 +1,40 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+import * as express from 'express'
+import * as cors from 'cors'
 
-require('dotenv').config()
+const REGION_SEOUL = 'asia-northeast3'
+
+const app = express()
+
+app.use(cors({ origin: true }))
 
 admin.initializeApp({
   serviceAccountId: process.env.SERVICE_ACCOUNT_ID,
 })
 
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-  const userId = 'hjy8697'
-  const additionalClaims = {
-    premiumAccount: true,
-  }
-
-  // const licenseId = req.query.licenseId
-
-  admin
-    .auth()
-    .createCustomToken(userId, additionalClaims)
-    .then((customToken) => {
-      // Send token back to client
-      console.log('@@customToken', customToken)
-    })
-    .catch((error) => {
-      console.log('Error creating custom token:', error)
-    })
+app.get('/hello', (req, res) => {
+  res.json('hello')
 })
+
+app.get('/customToken/:snsId', async (req, res) => {
+  const snsId = req.params.snsId as string
+
+  functions.logger.log(`customToken을 요청한 snsId: ${snsId}`)
+
+  if (snsId) {
+    admin
+      .auth()
+      .createCustomToken(snsId)
+      .then((customToken) => {
+        res.json({ customToken })
+      })
+      .catch((error) => {
+        console.log('Error creating custom token:', error)
+      })
+  } else {
+    throw Error('쿼리에 snsId가 없습니다.')
+  }
+})
+
+exports.app = functions.region(REGION_SEOUL).https.onRequest(app)
