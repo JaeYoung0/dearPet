255064af85fdc36c71eddba874817269fcf977bb
허재영 <hjy8697@gmail.com>
2022-03-13 03:35:05 +0900

    feat: kakao login

--- src/screens/Login/useAuth.ts
+++ src/screens/Login/useAuth.ts
@@ -1,57 +1,56 @@
-import React, { useEffect } from 'react'
-import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin'
+import React, { useEffect, useCallback } from 'react'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
 import { GOOGLE_CLIENT_ID } from '@env'
-import auth from '@react-native-firebase/auth'
-import { useRecoilState } from 'recoil'
-import { userStatus } from '@/modules/user/atoms'
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth'
+import { useMeState } from '@/modules/user/atoms'
 import { UserModel } from '@/server/users/model'
 import * as UserService from '@/server/users/service'
-import { handleSignInError } from './errorHandler'
+import { handleGoogleLoginError } from './errorHandler'
+import {
+  KakaoOAuthToken,
+  KakaoProfile,
+  getProfile as getKakaoProfile,
+  login as kakaoOAuthLogin,
+  logout as kakaoOAuthLogout,
+  unlink,
+} from '@react-native-seoul/kakao-login'
+import axios from 'axios'
+import useAppleLogin from './useAppleLogin'
+
+const customToken = async (snsId: string) => {
+  const { data } = await axios.get<{ customToken: string }>(
+    `https://asia-northeast3-noders-1.cloudfunctions.net/app/customToken/${snsId}`
+  )
+  return data.customToken
+}
 
 const useAuth = () => {
-  const [me, setMe] = useRecoilState(userStatus)
+  const { me, setMe } = useMeState()
 
-  async function googleLogin() {
+  const { appleLogin } = useAppleLogin()
+
+  async function findOrCreateUser(payload: Pick<FirebaseAuthTypes.User, 'uid' | 'displayName' | 'email' | 'photoURL'>) {
+    const { uid, displayName, photoURL, email } = payload
+
     try {
-      const { idToken } = await GoogleSignin.signIn()
-
-      const googleCredential = auth.GoogleAuthProvider.credential(idToken)
-      const UserCredential = await auth().signInWithCredential(googleCredential)
-      const googleAuthUser = UserCredential.user
-      console.log('@@googleAuthUser', googleAuthUser)
-
-      const { uid, displayName, photoURL, phoneNumber, email } = googleAuthUser
       const foundUser = (await UserService.getUser(uid)) as UserModel
 
       if (!foundUser) {
-        await UserService.createUser({
-          licenseId: uid,
-          displayName: displayName ?? '',
-          photoURL: photoURL ?? '',
-          phoneNumber: photoURL ?? '',
-          email: email ?? '',
-        })
-        setMe({
+        const command = {
           licenseId: uid,
           displayName: displayName ?? '',
           photoURL: photoURL ?? '',
           email: email ?? '',
-          phoneNumber: phoneNumber ?? '',
-        })
+        }
+        await UserService.createUser(command)
+        setMe(command)
       } else {
         setMe(foundUser)
       }
     } catch (error) {
-      handleSignInError(error)
+      console.error(error)
     }
   }
-
-  const socialLogin = {
-    google: googleLogin,
-    kakao: () => {},
-    naver: () => {},
-    apple: () => {},
-  } as const
 
   useEffect(() => {
     GoogleSignin.configure({
@@ -59,6 +58,53 @@ const useAuth = () => {
       offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
     })
   }, [])
+
+  const googleLogin = useCallback(async () => {
+    try {
+      const { idToken } = await GoogleSignin.signIn()
+
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken)
+      const UserCredential = await auth().signInWithCredential(googleCredential)
+      console.log('@@UserCredential', UserCredential)
+
+      const googleAuthUser = UserCredential.user
+
+      const { uid, displayName, photoURL, email } = googleAuthUser
+      findOrCreateUser({
+        uid,
+        displayName,
+        photoURL,
+        email,
+      })
+    } catch (error) {
+      handleGoogleLoginError(error)
+    }
+  }, [])
+
+  const kakaoLogin = useCallback(async () => {
+    await kakaoOAuthLogin()
+    const kakaoProfile = (await getKakaoProfile()) as KakaoProfile
+    const { profileImageUrl, email, nickname, id: kakaoId } = kakaoProfile
+
+    const token = await customToken(`kakao:${kakaoId}`)
+    const credential = await auth().signInWithCustomToken(token)
+
+    const uid = credential.user.uid
+
+    findOrCreateUser({
+      uid,
+      displayName: nickname,
+      email,
+      photoURL: profileImageUrl,
+    })
+  }, [])
+
+  const socialLogin = {
+    google: googleLogin,
+    kakao: kakaoLogin,
+    naver: () => {},
+    apple: () => {},
+  } as const
 
   return { me, socialLogin }
 }
